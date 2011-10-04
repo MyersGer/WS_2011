27071683c003f95f520a92a88cb9d0fa243a2d38
André H <myersger@googlemail.com>
2011-10-04 16:05:15 +0200

    Hauptscript + Eulerverfahren

--- MTP/01/mtp0101.m
+++ MTP/01/mtp0101.m
@@ -1,8 +1,3 @@
 function z = mtp0101(x, y)
     z = 10-500 * y + 5000 * x;
 end
-
-function [y, z] = mtp0101_ana(x)
-    y = 10*x+ exp(-500);
-    z = x;
-end
---
+++ MTP/01/mtp0101_ana.m
@@ -0,0 +1,4 @@
+function [z, y] = mtp0101_ana(x)
+    y = 10*x+ exp(-500);
+    z = x;
+end
