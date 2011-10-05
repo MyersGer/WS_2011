23abf1b4ec71b72271813edd2cb2f3d62e82467c
Hexren <oliver@steenbuck.org>
2011-10-05 15:51:28 +0200

    Protkoll add

--- MTP/01/spiff.m
+++ MTP/01/spiff.m
@@ -1,5 +1,5 @@
 [vec_ana_x, vec_ana_y] = mtp0101_ana_iterate(0.001, 0.2);
-[vec_ode45_x, vec_ode45_y] = ode45(@mtp0101, [0,0.2], [1]);
+[vec_ode45_x, vec_ode45_y] = ode1(@mtp0101, [0,0.2], [1]);
 [vec_eulerexpl_x, vec_eulerexpl_y] = euler_expl(0.003, 0.2);
 
 hold on;
