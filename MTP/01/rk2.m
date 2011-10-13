0d73df1615bd1645feaaeab4e83c41fec17364ba
Hexren <oliver@steenbuck.org>
2011-10-13 14:47:38 +0200

    mod: MT prak1 add iterationsgleichungen aufg 1

--- MTP/01/rk2.m
+++ MTP/01/rk2.m
@@ -5,7 +5,9 @@ function [x, y] = rk2(h, xend, f)
     
     vec_range = [0:h:xend];
     for ska_cur_x=vec_range
-        ska_y_i = ska_y_i + h * f(ska_cur_x + h / 2 , ska_y_i + h/2 * f(ska_cur_x, ska_y_i)); 
+        k1 = f(ska_cur_x, ska_y_i);
+        k2 = f(ska_cur_x + h, ska_y_i + h * k1);
+        ska_y_i = ska_y_i + h/2 * (k1 + k2);
         vec_xtmp = [vec_xtmp, ska_cur_x];
         vec_ytmp = [vec_ytmp, ska_y_i];
     end
