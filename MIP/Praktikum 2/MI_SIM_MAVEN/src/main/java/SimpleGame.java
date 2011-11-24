784c17aef5de9caf18e2f30b411bd0b979cdd989
André H <myersger@googlemail.com>
2011-11-24 21:24:24 +0100

    Framebegrenzung zum Abbilden realer Geschwindigkeit

--- MIP/Praktikum 2/MI_SIM_MAVEN/src/main/java/SimpleGame.java
+++ MIP/Praktikum 2/MI_SIM_MAVEN/src/main/java/SimpleGame.java
@@ -6,6 +6,7 @@ import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.tiled.TiledMap;
 
@@ -15,11 +16,18 @@ public class SimpleGame extends BasicGame {
 	private List<Agent> agents = new ArrayList<Agent>();
 	private static Logger logger;
 	
-	private static final String TILED_MAP_LOCATION = "maps/tile_map_campus_example.tmx";
+	private static final String TILED_MAP_LOCATION = "map_campus_berliner_tor.tmx";
 	private static final String TILED_RESOURCE_LOCATION = "maps";
+	
+	private static final int AGENT_SPEED_KMH = 5;
+	
+	
+	long lastTime = System.nanoTime();//, beforeTime, afterTime, timeDiff, sleepTime; 
 
 	public SimpleGame() {
 		super("Slick2DPath2Glory - SimpleGame");
+		
+		System.out.println(System.getProperty("user.dir"));
 	}
 
 	@Override
@@ -34,15 +42,45 @@ public class SimpleGame extends BasicGame {
 	@Override
 	public void update(GameContainer gc, int delta) throws SlickException {
 		logger.debug("update");
+		processInput(gc.getInput());		
+		
 		for (Agent agent : agents) {
-			agent.doTurn();
+			//agent.doTurn();
 		}
 		
+		double stepTime = Math.ceil(1/((AGENT_SPEED_KMH * 1000) / 3600000F)); // ms für einen Meter
+		
+		long currentTime = System.nanoTime();
+		long timeDiff = currentTime-lastTime;
+		
+		long sleepTime = (long) (stepTime - (timeDiff/1000000L)); //zu wartende Zeit in ms
+	
+		try{
+			Thread.sleep(sleepTime); //Zeitdifferenz aussitzen
+		}
+		catch(Exception ex){} //don't panic 
+		
+		lastTime = System.nanoTime();
+	}
+
+	private void processInput(Input in) {
+				if(in.isKeyDown(Input.KEY_DOWN)){
+					
+				}
+				else if (in.isKeyDown(Input.KEY_UP)){
+					
+				}
+				else if (in.isKeyDown(Input.KEY_LEFT)){
+					
+				}
+				else if (in.isKeyDown(Input.KEY_RIGHT)){
+					
+				}
 	}
 
 	public void render(GameContainer gc, Graphics g) throws SlickException {
+	
 		map.render(0, 0, 0, 0, map.getHeight(), map.getWidth());
-
 	}
 
 	public static void main(String[] args) throws SlickException {
