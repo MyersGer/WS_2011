8d1fbe89124a4fce5e422f9fd7333d6b9d0d0f31
Hexren <oliver@steenbuck.org>
2011-11-30 08:29:53 +0100

    mod: TTP3 Client siplayer

--- TT1/Prak2/lab3/src/main/java/Server.java
+++ TT1/Prak2/lab3/src/main/java/Server.java
@@ -2,7 +2,7 @@ import javax.sip.DialogTerminatedEvent;
 import javax.sip.RequestEvent;
 
 
-public class Server implements IMessageProcessor{
+public class Server{
 	
 	private SipLayer sipLayer;
 	
@@ -15,44 +15,7 @@ public class Server implements IMessageProcessor{
 
 
 
-	public void start() {
-		//startListening in sip
-	}
-
-
-
-	public void processMessage(String sender, String message) {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-
-	public void processError(String errorMessage) {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-
-	public void processInfo(String infoMessage) {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-
-	public void processDialogTerminated(DialogTerminatedEvent diaTermEv) {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-
-	public void processRequestEvent(RequestEvent requestEvent) {
-		// TODO Auto-generated method stub
-		
-	}
+	
 	
 	
 }
