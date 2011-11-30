90ddf6f7a5ec49e82a99e209be6c76a5ac24e91b
Armin Steudte <armin.steudte@googlemail.com>
2011-11-30 13:07:24 +0100

    IGMPListener und Sender implementiert

---
+++ TT1/Prak2/steudte_noetzel/TTP2/src/main/java/org/example/ttp2/IGMPListener.java
@@ -0,0 +1,87 @@
+package org.example.ttp2;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
+import org.apache.log4j.Logger;
+
+/**
+ * 
+ * @author Armin
+ *
+ */
+public class IGMPListener extends IGMPComponent{
+	
+	// Name des Loggers
+	public static final String TAG = "IGMPListener";
+	
+	// Loggerinstanz
+	private static final Logger LOGGER = Logger.getLogger(TAG);
+	
+	/**
+	 * Initialisiert den MulticastSocket 
+	 * 
+	 * @param ip			IPAdresse der Multicastgruppe
+	 * @param port			Port auf den 
+	 * @throws IOException  Fehler beim erzeugen des IPAdressen-Objekts oder Port
+	 */
+	protected void initialize(InetAddress ip, int port) throws IOException{
+		
+		// Socket anlegen und Gruppe joinen
+		mSocket = new MulticastSocket(port);
+		mSocket.joinGroup(mcastAdr);
+		
+		// IP und Port für später speichern
+		mcastAdr = ip;
+		this.port = port;
+		
+	}
+	
+	@Override
+	public void run() {
+		
+		LOGGER.debug("run()-Methode aufgerufen");
+		LOGGER.debug("isRunning: "+isRunning);
+		
+		while(isRunning) {
+			
+			LOGGER.debug("Schleife erreicht");
+			
+			try {
+				
+				// Auf Daten warten
+				mSocket.receive(pack);
+				
+				//Empfangene Daten ausgeben
+				System.out.write(pack.getData(),0,pack.getLength());
+				System.out.println();
+				
+			} catch (IOException e) {
+				
+				LOGGER.error("Fehler beim Lesen aus dem MulticastSocket: "+e);
+				
+				stop();
+				
+			}
+			
+			
+		}
+		
+		LOGGER.debug("Schleife beendet");
+		
+		try {
+			
+			mSocket.leaveGroup(mcastAdr);
+			
+		} catch (IOException e) {
+			LOGGER.error("Fehler beim verlassend er Multicastgruppe: "+e);
+		} finally {
+			mSocket.close();
+		}
+		
+		LOGGER.debug("MulticastSocket abgebaut");
+
+	}
+
+}
---
+++ TT1/Prak2/steudte_noetzel/TTP2/src/main/java/org/example/ttp2/IGMPSender.java
@@ -0,0 +1,101 @@
+package org.example.ttp2;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
+import org.apache.log4j.Logger;
+
+public class IGMPSender extends IGMPComponent{
+	
+	// Name des Loggers
+	public static final String TAG = "IGMPSender";
+	// Loggerinstanz
+	private static final Logger LOGGER = Logger.getLogger(TAG);
+//	private static final byte TTL = 1;
+	
+	
+	public IGMPSender() {
+		
+		super();
+		
+		buf = "Hallo von Noetzel Steudte".getBytes();
+		pack = new DatagramPacket(buf, buf.length);
+	}
+	
+	/**
+	 * Initialisiert den MulticastSocket 
+	 * 
+	 * @param ip			IPAdresse der Multicastgruppe
+	 * @param port			Port auf den 
+	 * @throws IOException  Fehler beim erzeugen des IPAdressen-Objekts oder Port
+	 */
+	protected void initialize(InetAddress ip, int port) throws IOException{
+		
+		// Socket anlegen
+		mSocket = new MulticastSocket();
+		
+		// Datagram mit Zielsocket versehen
+		pack.setAddress(ip);
+		pack.setPort(port);
+		
+		// IP und Port für später speichern
+		mcastAdr = ip;
+		this.port = port;
+		
+	}
+	
+	@Override
+	public void run() {
+		
+		LOGGER.debug("run()-Methode aufgerufen");
+		LOGGER.debug("isRunning: "+isRunning);
+		
+		while(isRunning) {
+			
+			LOGGER.debug("Schleife erreicht");
+
+			try {
+				
+//				LOGGER.debug(mcastAdr);
+//				LOGGER.debug(port);
+//				LOGGER.debug(new String(buf));
+				
+				mSocket.send(pack);
+				LOGGER.debug("Nachricht erfolgreich gesendet: "+buf);
+				Thread.sleep(2000);
+				
+			} catch (IOException e) {
+				
+				LOGGER.error("Fehler beim Senden der Nachrichten an die MulticastGruppe: "+e);
+				stop();
+				
+			} catch (InterruptedException e) {
+				
+				LOGGER.error("Fehler beim Sleep des Threads: "+e);
+				stop();
+				
+			}
+			
+		}
+		
+		LOGGER.debug("Schleife beendet");
+		
+		try {
+			
+			mSocket.leaveGroup(mcastAdr);
+			
+		} catch (IOException e) {
+			LOGGER.error("Fehler beim verlassend er Multicastgruppe: "+e);
+		} finally {
+			mSocket.close();
+		}
+		
+		LOGGER.debug("MulticastSocket abgebaut");
+		
+	}
+
+	
+	
+}
