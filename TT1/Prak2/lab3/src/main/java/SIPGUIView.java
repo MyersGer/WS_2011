5d147334ef8367b230a8073f1475e02238156749
Hexren <oliver@steenbuck.org>
2011-12-05 11:52:36 +0100

    test for merge

--- TT1/Prak2/lab3/src/main/java/SIPGUIView.java
+++ TT1/Prak2/lab3/src/main/java/SIPGUIView.java
@@ -4,17 +4,12 @@
 
 package sipgui;
 
-import org.jdesktop.application.Action;
-import org.jdesktop.application.ResourceMap;
-import org.jdesktop.application.SingleFrameApplication;
-import org.jdesktop.application.FrameView;
-import org.jdesktop.application.TaskMonitor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.Timer;
+
 import javax.swing.Icon;
 import javax.swing.JDialog;
-import javax.swing.JFrame;
+import javax.swing.Timer;
 
 /**
  * The application's main frame.
