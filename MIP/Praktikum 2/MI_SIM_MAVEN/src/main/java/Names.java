6ed7576dd88e5efcbebb9e3fe887ff664461972e
Hexren <oliver@steenbuck.org>
2011-12-07 13:16:50 +0100

    add: MIP2 add names interface

---
+++ MIP/Praktikum 2/MI_SIM_MAVEN/src/main/java/Names.java
@@ -0,0 +1,8 @@
+
+public interface Names {
+	public static final String EB_SECURITY = "Sicherheit";
+	public static final String EB_FUN = "Vergnuegen";
+	public static final String EB_SOZIOLOGY = "Soziologie";
+	public static final String EB_PRODUCTIVITY = "Produktivitaet";
+	public static final String EB_OVERALL = "Gesamt";
+}
