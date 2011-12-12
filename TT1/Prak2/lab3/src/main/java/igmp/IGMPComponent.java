9ed2bc8a9c1d1bb772f41430247843cf842c31c5
Oliver Steenbuck <git@hexren.net>
2011-12-13 00:03:31 +0100

    add: TTP3 rm old siplayer code

---
+++ TT1/Prak2/lab3/src/main/java/igmp/IGMPComponent.java
@@ -0,0 +1,48 @@
+package igmp;
+
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
+/**
+ * 
+ * @author Armin
+ *
+ */
+public abstract class IGMPComponent implements Runnable {
+
+	// Größe der erwarteten Pakete
+	public static final int PACKETSIZE = 257;
+	
+	protected MulticastSocket mSocket;
+	protected InetAddress mcastAdr;
+	protected int port;
+	
+	protected byte buf[];
+	protected DatagramPacket pack;
+	
+	protected boolean isRunning;
+	
+	
+	
+	public IGMPComponent() {
+		
+		mSocket = null;
+		isRunning = true;
+		
+		// Buffer für Pakete und passendes Datagramobjekt erzeugen
+		buf = new byte[PACKETSIZE];
+		
+	}
+	
+	/**
+	 * Setzt das isRunning-Flag und sorgt dafür, dass
+	 * der Thread stoppt wird.
+	 */
+	public void stop() {
+		
+		this.isRunning = false;
+		
+	}
+	
+}
