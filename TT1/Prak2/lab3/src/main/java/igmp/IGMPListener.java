fd22eaf191bd15e2f1db2e692aa620d749ac7f72
Hexren <oliver@steenbuck.org>
2012-01-04 09:08:44 +0100

    add: TTP1 sh add leave to multicast

--- TT1/Prak2/lab3/src/main/java/igmp/IGMPListener.java
+++ TT1/Prak2/lab3/src/main/java/igmp/IGMPListener.java
@@ -7,89 +7,90 @@ import java.net.MulticastSocket;
 
 import org.apache.log4j.Logger;
 
+public class IGMPListener extends IGMPComponent {
 
-public class IGMPListener extends IGMPComponent{
-	
 	private IGMPListenerObserver observer = null;
-	
-	public void setObserver(IGMPListenerObserver observer){
+
+	public void setObserver(IGMPListenerObserver observer) {
 		this.observer = observer;
 	}
-	
+
 	// Name des Loggers
 	public static final String TAG = "IGMPListener";
-	
+
 	// Loggerinstanz
 	private static final Logger LOGGER = Logger.getLogger(TAG);
-	
+
 	/**
-	 * Initialisiert den MulticastSocket 
+	 * Initialisiert den MulticastSocket
 	 * 
-	 * @param ip			IPAdresse der Multicastgruppe
-	 * @param port			Port auf den 
-	 * @throws IOException  Fehler beim erzeugen des IPAdressen-Objekts oder Port
+	 * @param ip
+	 *            IPAdresse der Multicastgruppe
+	 * @param port
+	 *            Port auf den
+	 * @throws IOException
+	 *             Fehler beim erzeugen des IPAdressen-Objekts oder Port
 	 */
-	public void initialize(InetAddress ip, int port) throws IOException{
-		
+	public void initialize(InetAddress ip, int port) throws IOException {
+
 		// Socket anlegen und Gruppe joinen
 		mSocket = new MulticastSocket(port);
 		mcastAdr = ip;
-		
+
 		mSocket.joinGroup(mcastAdr);
-		
+
 		// IP und Port für später speichern
 		mcastAdr = ip;
 		this.port = port;
 		pack = new DatagramPacket(buf, buf.length);
-		
+
 	}
-	
+
 	@Override
 	public void run() {
-		
+
 		LOGGER.debug("run()-Methode aufgerufen");
-		LOGGER.debug("isRunning: "+isRunning);
-		
-		while(isRunning) {
-			
-			
-			
+		LOGGER.debug("isRunning: " + isRunning);
+
+		while (isRunning) {
+
 			try {
-				
+
 				// Auf Daten warten
 				mSocket.receive(pack);
-				
-				//Empfangene Daten ausgeben
+
+				// Empfangene Daten ausgeben
 				String message = new String(pack.getData());
 				LOGGER.trace("Message: " + message);
-				if(null!=observer)
+				if (null != observer)
 					observer.addMessage(message);
-				
+
 			} catch (IOException e) {
-				
-				LOGGER.error("Fehler beim Lesen aus dem MulticastSocket: "+e);
-				
+
+				LOGGER.error("Fehler beim Lesen aus dem MulticastSocket: " + e);
+
 				stop();
-				
+
 			}
-			
-			
+
 		}
-		
+
 		LOGGER.debug("Schleife beendet");
-		
+
 		try {
-			
+
 			mSocket.leaveGroup(mcastAdr);
-			
+
 		} catch (IOException e) {
-			LOGGER.error("Fehler beim verlassend er Multicastgruppe: "+e);
+			LOGGER.error("Fehler beim verlassend er Multicastgruppe: " + e);
 		} finally {
 			mSocket.close();
 		}
-		
+
 		LOGGER.debug("MulticastSocket abgebaut");
 
 	}
+	
+	
 
 }
